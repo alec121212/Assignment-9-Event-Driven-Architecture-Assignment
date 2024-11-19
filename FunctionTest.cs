d1e715eed69f1825cb701b94b8197f77855c191d
alec121212 <49066342+alec121212@users.noreply.github.com>
2024-11-18 21:25:34 -0500

    Add files via upload

--- FunctionTest.cs
+++ FunctionTest.cs
@@ -9,7 +9,8 @@ public class FunctionTest
     [Fact]
     public void TestToUpperFunction()
     {
-        
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
         // Invoke the lambda function and confirm the string was upper cased.
         var function = new Function();
         var context = new TestLambdaContext();
